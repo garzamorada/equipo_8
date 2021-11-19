84d5323ed129c28046ade175d018599e5d234bfd
Andres <garzamorada@gmail.com>
2021-11-19 16:36:50 -0300

    mergeado a mano el 4

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from src.extra_b import *
+from src.ejercicio_3 import *
 
 
 cadenaExtraB=input('ingrese su cadena para contar caractares: ')
 contarCaracteres(cadenaExtraB)
 
+cadenaIni=input('Ingrese la cadena inicial: ')
+indice=input('Ingrese el indice: ')
+nvoCaracter=input('Ingrese el nuevo caracter: ')
+cambiaCaracter(cadenaIni, indice, nvoCaracter)
---
+++ src/ejercicio_3.py
@@ -0,0 +1,10 @@
+# 3- Los strings son inmutables, escribir una funcion que reciba 
+#    un string, un indice y una letra a modificar de ese string y 
+#    que devuelve el string modificado.""")
+
+def cambiaCaracter(cadenaIni, indice, nvoCaracter):
+    #cadenaIni=input('Ingrese la cadena inicial: ')
+    #indice=input('Ingrese el indice: ')
+    #nvoCaracter=input('Ingrese el nuevo caracter: ')
+    i=int(indice)
+    print(cadenaIni[0:i]+nvoCaracter[0:1]+cadenaIni[i+1:])
